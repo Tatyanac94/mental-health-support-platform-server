0afc32d164051d312ce7c9a819f9acda6f8c3897
Tatyanac94 <ms.cuttino94@gmail.com>
2024-10-12 21:30:49 -0400

    update format

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -10,6 +10,7 @@ export const errorHandler = (err: CustomError, req: Request, res: Response, next
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
 
+  
   res.status(statusCode).json({
     status: 'error',
     statusCode,
